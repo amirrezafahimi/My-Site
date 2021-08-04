59b279262a7c7ca1ed8a67c15b5108564f059e8e
Amirreza Fahimi <amirrezafahimi98@gmail.com>
2021-08-04 14:24:56 +0430

    added comment section to my site

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 import django.views
-from django.shortcuts import render, get_object_or_404
+from .forms import CommentForm
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Post
 
@@ -24,11 +27,30 @@ class AllPostsView(django.views.generic.ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(django.views.generic.DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(django.views.View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
