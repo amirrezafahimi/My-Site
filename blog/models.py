b37047dc3dde88be83831ffa7fdf4407371c95eb
Amirreza Fahimi <amirrezafahimi98@gmail.com>
2021-08-04 13:15:45 +0430

    changed functional views to django's class based views

--- blog/models.py
+++ blog/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
